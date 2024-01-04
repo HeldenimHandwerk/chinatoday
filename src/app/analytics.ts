9cc137d115dfb4b914bb82f4696d2040430cf091
Saif Shahwan <Saif_shahwan@hotmail.com>
2024-01-04 02:24:47 +0100

    Commented out Google Analytics code

--- src/app/analytics.ts
+++ src/app/analytics.ts
@@ -1,24 +1,24 @@
-"use client";
+// "use client";
 
-// src/analytics.js
-import { useEffect } from "react";
-import ReactGA from "react-ga";
+// // src/analytics.js
+// import { useEffect } from "react";
+// import ReactGA from "react-ga";
 
-export const initGA = () => {
-  const trackingId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS || "G-XJPYLQV9V2";
-  ReactGA.initialize(trackingId);
-};
+// export const initGA = () => {
+//   const trackingId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS || "G-XJPYLQV9V2";
+//   ReactGA.initialize(trackingId);
+// };
 
-export const logPageView = () => {
-  ReactGA.set({ page: window.location.pathname });
-  ReactGA.pageview(window.location.pathname + window.location.search);
-};
+// export const logPageView = () => {
+//   ReactGA.set({ page: window.location.pathname });
+//   ReactGA.pageview(window.location.pathname + window.location.search);
+// };
 
-export default function GoogleAnalytics() {
-  useEffect(() => {
-    initGA();
-    logPageView();
-  }, []);
+// export default function GoogleAnalytics() {
+//   useEffect(() => {
+//     initGA();
+//     logPageView();
+//   }, []);
 
-  return null;
-}
+//   return null;
+// }
