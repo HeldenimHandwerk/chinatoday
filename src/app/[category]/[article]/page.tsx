ed1e99e40d1893440c3fe99b57bc5b90f0f7003f
Saif Shahwan <Saif_shahwan@hotmail.com>
2024-01-28 00:48:25 +0100

    Refactor ck rendering in page.tsx

--- src/app/[category]/[article]/page.tsx
+++ src/app/[category]/[article]/page.tsx
@@ -163,7 +163,7 @@ const Page: React.FC<Props> = async ({ params: { article } }) => {
             </div>
           )}
           <TextToSpeechButton text={text} />
-          <div>{ck}</div>
+          <div dangerouslySetInnerHTML={{ __html: ck }} />
           {!image && (
             <div className="mb-4 rounded-lg bg-red-200 p-4 text-red-800 shadow">
               <h1 className="text-2xl font-bold text-gray-900">{title}</h1>
